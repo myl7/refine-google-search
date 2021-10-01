3831d42f8298c4d238ea5181a079895add325021
myl7 <myl@myl.moe>
2021-10-02 03:43:34 +0800

    feat: Provide perfect two column

--- refine-google-search.user.js
+++ refine-google-search.user.js
@@ -20,60 +20,52 @@
 // @author           myl7
 // ==/OpenUserJS==
 
+const colWidth = '535px'
+
+let style = ''
+
 // Remove "translate this page" button
 document.querySelectorAll('a.fl.iUh30').forEach(e => e.remove())
 
 // Two column display
 const resRoot = document.querySelector('.v7W49e')
-const res = resRoot.children
 let items = []
-let hasLast = false
-for (let i = 0; i < res.length; i++) {
-  if (['SCRIPT', 'SPAN', 'STYLE'].includes(res[i].tagName)) {
-    continue
-  } else if (res[i].className === 'g') {
-    items.push(res[i])
-  } else if (res[i].children.length === 1 && res[i].children[0].className === 'g') {
-    if (i === res.length - 1) {
-      // The last search result is wrapped in a div
-      items.push(res[i].children[0])
-      res[i].remove()
-      hasLast = true
-    } else {
-      // Thicker varient of search result
-      items.push(res[i])
-    }
-  } else {
-    items = []
+for (let e of resRoot.children) {
+  if (!['SCRIPT', 'SPAN', 'STYLE'].includes(e.tagName)) {
+    e.style.width = colWidth
+    items.push(e)
   }
 }
+style += `div.g:not(.jNVrwc.Y4pkMc) { width: ${colWidth} }`
 
-for (let i = 0; i < (hasLast ? items.length - 1 : items.length); i++) {
-  items[i].remove()
+const floatWin = document.querySelector('div.TQc1id.hSOk2e.rhstc4')
+if (floatWin) {
+  floatWin.style.marginLeft = '0'
+  items.splice(1, 0, floatWin)
 }
+
+items.forEach(e => e.remove())
 
 const itemBox = document.createElement('div')
 items.forEach(e => itemBox.appendChild(e))
 itemBox.style.display = 'grid'
 itemBox.style.gridTemplateColumns = '1fr 1fr'
+itemBox.style.gridColumnGap = '10px'
 itemBox.style.marginBottom = '44px'
 resRoot.appendChild(itemBox)
 
 // Decrease title font size
-let style = ''
 style += 'h3 { font-size: 16px !important }'
 
-// Change bottom relative search word display
+// Change bottom relative search style
 style += 'div.eqAnXb { margin-bottom: -20px }'
 style += 'div.s75CSd.OhScic.AB4Wff { font-size: 14px }'
 style += 'a.k8XOCe.R0xfCb.VCOFK.s8bAkb { background-color: inherit; min-height: 0; margin: 0; line-height: 24px }'
 document.querySelectorAll('div.aXBZVd.zVq10e').forEach(e => e.remove())
 
-// Change "other searches" word display
+// Change middle other search style
 style += 'div.iDjcJe.IX9Lgd.wwB5gf { font-size: 14px }'
-
-// Fix result block width
-style += 'div.g:not(.jNVrwc.Y4pkMc) { width: 540px }'
+style += `div.E9iWlb.KFFQ0c.xKf9F { width: ${colWidth} }`
 
 // CSS inject helper
 const addStyle = style => {
